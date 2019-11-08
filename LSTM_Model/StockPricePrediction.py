06b21eae3b8107292c3ca517a32a05b109cd9c46
Subham <subhamrdn405@gmail.com>
2019-11-09 00:50:55 +0530

    structure modules

--- LSTM_Model/StockPricePrediction.py
+++ LSTM_Model/StockPricePrediction.py
@@ -14,7 +14,7 @@ import preprocessing
 np.random.seed(7)
 
 # IMPORTING DATASET 
-dataset = pd.read_csv('apple_share_price.csv', usecols=[1,2,3,4])
+dataset = pd.read_csv('../Data/apple_share_price.csv', usecols=[1,2,3,4])
 dataset = dataset.reindex(index = dataset.index[::-1])
 
 # CREATING OWN INDEX FOR FLEXIBILITY
